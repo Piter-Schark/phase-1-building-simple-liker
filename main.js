43b061c23da841dd348ac18d9f452277063f4ae9
Peter <pschoenbauer@gmail.com>
2023-06-17 19:57:21 -0400

    hard lab - did with a bunch of help

--- main.js
+++ main.js
@@ -1,11 +1,43 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+let likeButtons = document.querySelectorAll(".like-glyph")
 
 // Your JavaScript code goes here!
 
 
+function clickLikeButtons(e){
+ 
+    
+      const heart = e.target
+      console.log (heart)
+      mimicServerCall("bogusUrl")
+      .then(function () {
+        if (heart.innerText === EMPTY_HEART){
+          heart.innerText = FULL_HEART
+          heart.className = "activated-heart"
+        console.log ("empty to full") 
+        }
+        else {
+          heart.innerText = EMPTY_HEART
+          heart.className = ""
+          console.log ("full to empty")
+        }})
+  
+     .catch((error) => {
+     document.getElementById("modal").className = ""
+     setTimeout(function(){
+      document.getElementById("modal").className = "hidden" 
+     },3000);
+     return error
+    })
+    
+  
+}
 
+for (let btn of likeButtons){
+btn.addEventListener('click',(clickLikeButtons) )
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
